c9c213c311e56957e44395a33b0e232e9795766d
Tao.T <1120135081@qq.com>
2020-06-30 22:29:21 +0800

    获取资源目录

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -12,7 +12,18 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        for item in items {
+            if item.hasSuffix("nssl") {
+                
+            }
+        }
     }
+    
+    
 
 }
 
