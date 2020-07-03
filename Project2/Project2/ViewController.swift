c9c9e762bddbe783c52bbdb61f18230a7511f69f
Tao.T <1120135081@qq.com>
2020-07-03 13:16:55 +0800

    完成Day19

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Tao.T on 2020/7/3.
+//  Copyright © 2020 TaoTao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        button1.layer.borderWidth = 1
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        
+        button2.layer.borderWidth = 1
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        
+        button2.layer.borderWidth = 1
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        askQuestion()
+    }
+    
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[0]), for: .normal)
+    }
+}
+
