80ae001c5bb9ec63a2b723b20c9701375a4302b5
Tao.T <1120135081@qq.com>
2020-07-28 17:08:30 +0800

    完成Day39

---
+++ Project9/Project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Tao.T on 2020/7/23.
+//  Copyright © 2020 TaoTao. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
