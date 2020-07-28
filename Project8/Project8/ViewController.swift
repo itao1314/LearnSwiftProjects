363b92ef8fd26d5390ed25b5b675c91c022acc4a
Tao.T <1120135081@qq.com>
2020-07-28 10:56:37 +0800

    完成Day38

--- Project8/Project8/ViewController.swift
+++ Project8/Project8/ViewController.swift
@@ -17,7 +17,11 @@ class ViewController: UIViewController {
     var letterButtons = [UIButton]()
     var activatedButtons = [UIButton]()
     var solutions = [String]()
-    var score = 0
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
     var level = 1
 
     override func loadView() {
@@ -117,6 +121,8 @@ class ViewController: UIViewController {
                 let frame = CGRect(x: col * width, y: row * height, width: width, height: height)
                 letterButton.frame = frame
                 letterButton.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
+                letterButton.layer.borderWidth = 1
+                letterButton.layer.borderColor = UIColor.gray.cgColor
                 buttonsView.addSubview(letterButton)
                 letterButtons.append(letterButton)
             }
@@ -126,17 +132,85 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadLever()
     }
     
     func loadLever() {
-        
+        var clueString = ""
+        var solutionString = ""
+        var letterBits = [String]()
+        guard let levelFileURL = Bundle.main.url(forResource: "level\(level)", withExtension: "txt") else { return }
+        guard let levelContents = try? String(contentsOf: levelFileURL) else { return }
+        var lines = levelContents.components(separatedBy: "\n")
+        lines.shuffle()
+        for (index, line) in lines.enumerated() {
+            let parts = line.components(separatedBy: ": ")
+            let answer = parts[0]
+            let clue = parts[1]
+            
+            clueString += "\(index + 1). \(clue)\n"
+            
+            let solutionWord = answer.replacingOccurrences(of: "|", with: "")
+            solutionString += "\(solutionWord.count) letters\n"
+            solutions.append(solutionWord)
+            
+            let bits = answer.components(separatedBy: "|")
+            letterBits += bits
+            
+            cluesLabel.text = clueString.trimmingCharacters(in: .whitespacesAndNewlines)
+            answersLabel.text = solutionString.trimmingCharacters(in: .whitespacesAndNewlines)
+            letterBits.shuffle()
+            
+            if letterBits.count == letterButtons.count {
+                for i in 0 ..< letterButtons.count {
+                    letterButtons[i].setTitle(letterBits[i], for: .normal)
+                }
+            }
+        }
     }
     
-    @objc func submitTapped(_ sender: UIButton) {}
+    @objc func submitTapped(_ sender: UIButton) {
+        guard let answerText = currentAnswer.text else { return }
+        if let solutionPosition = solutions.firstIndex(of: answerText) {
+            activatedButtons.removeAll()
+            var splitAnswers = answersLabel.text?.components(separatedBy: "\n")
+            splitAnswers?[solutionPosition] = answerText
+            answersLabel.text = splitAnswers?.joined(separator: "\n")
+            currentAnswer.text = ""
+            score += 1
+            if score % 7 == 0 {
+                let ac = UIAlertController(title: "Well done!", message: "Are you ready for the next level?", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "Let's go!", style: .default, handler: levelUp))
+                present(ac, animated: true)
+            }
+        }
+    }
     
-    @objc func clearTapped(_ sender: UIButton) {}
+    func levelUp(action: UIAlertAction) {
+        level += 1
+        solutions.removeAll(keepingCapacity: true)
+
+        loadLever()
+        
+        for btn in letterButtons {
+            btn.isHidden = false
+        }
+    }
     
-    @objc func letterTapped(_ sender: UIButton) {}
+    @objc func clearTapped(_ sender: UIButton) {
+        currentAnswer.text = ""
+        for btn in activatedButtons {
+            btn.isHidden = false
+        }
+        activatedButtons.removeAll()
+    }
+    
+    @objc func letterTapped(_ sender: UIButton) {
+        guard let buttonTitle = sender.titleLabel?.text else { return }
+        currentAnswer.text = currentAnswer.text?.appending(buttonTitle)
+        activatedButtons.append(sender)
+        sender.isHidden = true
+    }
 
 
 }
