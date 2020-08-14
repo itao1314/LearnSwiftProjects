ccc6d162a1da873072b6c0a36b43d884b2fd3350
Tao.T <1120135081@qq.com>
2020-08-14 19:08:06 +0800

    初始化project14

---
+++ Project14/Project14/GameScene.swift
@@ -0,0 +1,24 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Tao.T on 2020/8/14.
+//  Copyright © 2020 TaoTao. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+       
+    }
+    
+    
+}
